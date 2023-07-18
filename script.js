64189046337986d1369b563eada5b33a802d2a9a
Ralph Saade <86687858+ralphsaade@users.noreply.github.com>
2023-07-19 00:10:15 +0200

    Adding some script for the tracker

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    let totalExpense = 0;
+  
+    $('#expense-form').on('submit', function(e) {
+      e.preventDefault();
+      
+      const expenseName = $('#expenseName').val();
+      const expenseAmount = $('#expenseAmount').val();
+      
+      if(expenseName.trim() === "" || expenseAmount.trim() === "") {
+        alert("Please fill in all the fields");
+      } else {
+        totalExpense += parseFloat(expenseAmount);
+  
+        const expenseRow = `<tr>
+            <td>${expenseName}</td>
+            <td>${expenseAmount}</td>
+            <td><button class="btn btn-danger delete">Delete</button></td>
+          </tr>`;
+  
+        $('#expenseTable tbody').append(expenseRow);
+        $('#totalExpense').text(totalExpense);
+        
+        $('#expenseName').val("");
+        $('#expenseAmount').val("");
+      }
+    });
+  
+    $('#expenseTable').on('click', '.delete', function() {
+      const amount = parseFloat($(this).closest('tr').children('td:nth-child(2)').text());
+      totalExpense -= amount;
+  
+      $(this).closest('tr').remove();
+      $('#totalExpense').text(totalExpense);
+    });
+  });
+  
